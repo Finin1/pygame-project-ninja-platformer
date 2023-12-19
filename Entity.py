77ecc9ad033a6ef9913ce2d9d49100407bf8e5a7
Finin <egor.fa_a@mail.ru>
2023-12-19 17:36:09 +0300

    Добавленны коментарии + убрано лишнее в Entity.py

--- Entity.py
+++ Entity.py
@@ -4,8 +4,8 @@ import typing
 
 class Entity():
     def __init__(self, health, pos: list, size: typing.Tuple, v: list, mass):
-        self.speed = v
-        self.position = pos
+        self.speed = v # list(v_x, v_y)
+        self.position = pos # list(x, y)
         self.MAX_HEALTH = health
         self.health = health
         self.MASS = mass
@@ -14,14 +14,10 @@ class Entity():
         self.is_alive = True
         self.is_collided = {'top': False, 'bottom': False, 'right': False, 'left': False}
 
-    def render(self, screen, objects): # куча проверок. Физика, состояние здоровья, скорость
+    def render(self, screen, objects): # Отрисовка, куча проверок. Физика, состояние здоровья, скорости
+        # Заготовка для проверки жива ли сущность
         if self.health <= 0:
             self.is_alive = False
-        # Гравитация
-        # if self.in_air:
-        #     # self.speed[1] += 10
-        # else:
-        #     self.speed[1] = 0
 
         self.check_collision(objects)
 
@@ -39,27 +35,27 @@ class Entity():
                 top_side = self.position[1]
                 bottom_side = self.position[1] + self.size[1]
                 
-                if platform.top <= bottom_side and platform.top >= top_side and self.is_collided['bottom'] == False:
+                if platform.top <= bottom_side and platform.top >= top_side and self.is_collided['bottom'] == False: # Нижная коллизия
                     modifier = abs(bottom_side - platform.top)
                     self.position[1] -= modifier 
-                    # self.in_air = False
                     self.is_collided['bottom'] = not self.is_collided['bottom'] 
-                elif platform.bottom >= top_side and platform.bottom <= bottom_side and self.is_collided['top'] == False:
+                elif platform.bottom >= top_side and platform.bottom <= bottom_side and self.is_collided['top'] == False: # Верхняя коллизия
                     modifier = abs(top_side - platform.bottom)
                     self.position[1] += modifier
                     self.speed[1] = 0
                     self.is_collided['top'] = not(self.is_collided['top'])
-                elif platform.right >= left_side and platform.right <= right_side and self.is_collided['left'] == False:
+                elif platform.right >= left_side and platform.right <= right_side and self.is_collided['left'] == False: # Левая коллизия
                     modifier = abs(left_side - platform.right)
                     self.position[0] += modifier
                     self.speed[0] = 0
                     self.is_collided['left'] = not(self.is_collided['left'])
-                elif platform.left <= right_side and platform.left >= left_side and self.is_collided['right'] == False:
+                elif platform.left <= right_side and platform.left >= left_side and self.is_collided['right'] == False: # Правая коллизия
                     modifier = abs(right_side - platform.left)
                     self.position[0] -= modifier 
                     self.speed[0] = 0
                     self.is_collided['right'] = not(self.is_collided['right'])
 
+                # Обнуление скоростей, если есть столкновение
                 if self.is_collided['top'] == 1:
                     self.speed[1] = 0
                 if self.is_collided['bottom'] == 1:
@@ -91,9 +87,10 @@ if __name__ == '__main__':
                 test_entity.speed[1] += 100
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 test_entity.speed[0] += 100
+        
         screen.fill((0, 0, 0))
-        hitboxes = []
 
+        # Отрисовка тестовых объектов
         for platform in platforms:
             pygame.draw.rect(screen, pygame.Color('blue'), platform)
         test_entity.render(screen, platforms)
