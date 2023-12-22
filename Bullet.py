a3529f76cb1a64597ca835e4bcc757ce39b08a8c
Ян Нестеров <116672081+YanL1ght@users.noreply.github.com>
2023-12-22 17:11:09 +0300

    Чуть-чуть поменял класс
    Эти изменения пригодятся позже

--- Bullet.py
+++ Bullet.py
@@ -1,15 +1,17 @@
 import pygame as pg
+# from weapon import Weapon
 
 
-class Bullet:  # класс пули
-    def __init__(self, x, y, radius, color, facing):  # создание пули по введённым параметрам
+class Bullet:
+    def __init__(self, x, y, radius, color, facing):
         self.x = x
         self.y = y
         self.radius = radius
+        self.width = radius * 2
+        self.height = radius * 2
         self.color = color
         self.facing = facing
         self.vel = 50 * facing
 
-    def render(self, sc):  # отрисовка пули
+    def render(self, sc):
         pg.draw.circle(sc, self.color, (self.x, self.y), self.radius)
-        
