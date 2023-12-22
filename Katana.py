695e9d65c923f282446b3d3787607069fcaf6667
Ян Нестеров <116672081+YanL1ght@users.noreply.github.com>
2023-12-22 19:48:09 +0300

    Update Katana.py

--- Katana.py
+++ Katana.py
@@ -9,15 +9,24 @@ class Katana:
         self.sprite = sprite
         self.sprite.mask = pg.mask.from_surface(sprite.image)
         self.center_pos = self.sprite.image.get_rect(center=(self.y, self.y))
-        self.incline = 105
-
-    def render(self, x, y):
-        self.sprite.rect.x = x
-        self.sprite.rect.y = y
-        pg.transform.rotate(self.sprite.image, int(self.incline))
+        self.incline = -45
+        self.for_rotate = False
+        self.facing = 1
+        self.sprite.image = pg.transform.rotate(self.sprite.image, int(self.incline))
         self.sprite.rect = self.sprite.image.get_rect(center=(self.sprite.rect.x, self.sprite.rect.y))
 
+    def render(self, x, y, face):
+        self.sprite.rect.x = x
+        self.sprite.rect.y = y
+
+        if self.for_rotate:
+            self.sprite.image = pg.transform.flip(self.sprite.image, False, True)
+            self.for_rotate = False
+
+        self.sprite.rect = self.sprite.image.get_rect(center=(self.sprite.rect.x, self.sprite.rect.y))
+        if face != self.facing:
+            self.sprite.image = pg.transform.flip(self.sprite.image, True, False)
+            self.facing *= -1
+
     def actived(self):
-        self.incline = 135
-        pg.time.wait(100)
-        self.incline = 45
+        self.for_rotate = True
