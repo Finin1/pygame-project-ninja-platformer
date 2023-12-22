b79b48eb4940cd40bd8e6ba4c8cef5e85cbab951
Ян Нестеров <116672081+YanL1ght@users.noreply.github.com>
2023-12-22 19:27:38 +0300

    я пытался

--- Katana.py
+++ Katana.py
@@ -1,17 +1,21 @@
 import pygame as pg
+import math
 
 
 class Katana:
     def __init__(self, x, y, sprite):
         self.x = x
         self.y = y
-        self.sprire = sprite
-        self.incline = 45
+        self.sprite = sprite
+        self.sprite.mask = pg.mask.from_surface(sprite.image)
+        self.center_pos = self.sprite.image.get_rect(center=(self.y, self.y))
+        self.incline = 105
 
     def render(self, x, y):
-        self.sprire.rect.x = x
-        self.sprire.rect.y = y
-        pg.transform.rotate(self.sprire.image, self.incline)
+        self.sprite.rect.x = x
+        self.sprite.rect.y = y
+        pg.transform.rotate(self.sprite.image, int(self.incline))
+        self.sprite.rect = self.sprite.image.get_rect(center=(self.sprite.rect.x, self.sprite.rect.y))
 
     def actived(self):
         self.incline = 135
