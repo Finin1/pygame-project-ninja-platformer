7072d681f4fb138584d8bb2ea89d479ad4111526
Finin <egor.fa_a@mail.ru>
2023-12-25 19:36:42 +0300

    Entity интегрирован в Player, добавлена гравитация и коллизия

--- Player.py
+++ Player.py
@@ -2,16 +2,15 @@ import pygame as pg
 from Bullet import Bullet
 from os import path
 from sys import exit
+
+from Entity import Entity
 from Katana import Katana
-# import Entity
 
 
-class Player:  # класс Игрока
-    def __init__(self, screen, win, x=500, y=500):  # инициализация класса
-        self.pos_x = x
-        self.pos_y = y
-        self.width = 50
-        self.height = 100
+
+class Player(Entity):  # Класс Игрока
+    def __init__(self, screen, win, health, speed: list, mass, pos=[500, 500], size=[50, 100]):  # инициализация класса
+        super().__init__(health, pos, size, speed, mass)
         self.screen = screen
         self.win_size = win
         self.body = None
@@ -19,10 +18,9 @@ class Player:  # класс Игрока
         self.jumpCount = 20
         self.bullets = []
         self.facing = 1
-        self.speed = 1
 
-    def render(self):  # отрисовка персонажа и пуль
-        pg.draw.rect(self.screen, pg.Color('white'), (self.pos_x, self.pos_y, self.width, self.height))
+    def render(self, objects):  # Отрисовка персонажа и пуль
+        super().render(self, objects)
 
         for bullet in self.bullets:
             if 0 < bullet.x < self.win_size[0]:
@@ -31,33 +29,39 @@ class Player:  # класс Игрока
             else:
                 self.bullets.pop(self.bullets.index(bullet))
 
-    def move(self, x=10):  # движение персонажа
+    def move(self, x=10):  # Движение персонажа
         keys = pg.key.get_pressed()
         if keys[pg.K_a]:
-            self.pos_x -= x * self.speed
+            self.is_collided['right'] = False 
+            self.is_collided['left'] = False 
+            self.speed_x = 1
+            self.pos_x -= x * self.speed_x
             self.facing = -1
         elif keys[pg.K_d]:
-            self.pos_x += x * self.speed
+            self.is_collided['right'] = False 
+            self.is_collided['left'] = False 
+            self.speed_x = 1
+            self.pos_x += x * self.speed_x
             self.facing = 1
 
-        katana.render(self.pos_x + 30 + (self.width // 2 * self.facing), self.pos_y + (self.height // 2), self.facing)
+        katana.render(self.pos_x + 30 + (self.width // 2 * self.facing), self.pos_y + (self.height // 2) + self.speed_y, self.facing)
 
-    def jump(self):  # прыжок персонажа
-        if self.isJump:
-            if self.jumpCount >= -20:
-                neg = 1
-                if self.jumpCount < 0:
-                    neg = -1
-                self.pos_y -= self.jumpCount ** 2 * 0.1 * neg
-                self.jumpCount -= 1
-            else:
-                self.isJump = False
-                self.jumpCount = 20
+    # def jump(self):  # прыжок персонажа
+    #     if self.isJump:
+    #         if self.jumpCount >= -20:
+    #             neg = 1
+    #             if self.jumpCount < 0:
+    #                 neg = -1
+    #             self.pos_y -= self.jumpCount ** 2 * 0.1 * neg
+    #             self.jumpCount -= 1
+    #         else:
+    #             self.isJump = False
+    #             self.jumpCount = 20
 
     def shot(self):  # выстрел персонажа
-        self.bullets.append(Bullet(self.pos_x, self.pos_y + (self.height // 2), 10, (255, 0, 0), self.facing))
+        self.bullets.append(Bullet(self.pos_x, self.pos_y + (self.height // 2) + self.speed_y, 10, (255, 0, 0), self.facing))
 
-    def lunge(self, x=300):  # одна из механик игры, выпад
+    def lunge(self, x=300):  # Одна из механик игры, выпад
         self.pos_x += x * self.facing
 
     def attack(self):
@@ -66,7 +70,7 @@ class Player:  # класс Игрока
 
 def load_image(name):
     fullname = path.join('data\images', name)
-    # если файл не существует, то выходим
+    # Если файл не существует, то выходим
     if not path.isfile(fullname):
         print(f"Файл с изображением '{fullname}' не найден")
         exit()
@@ -74,13 +78,15 @@ def load_image(name):
     return img
 
 
-if __name__ == '__main__':  # демонстрация работы класса
+if __name__ == '__main__':  # Демонстрация работы класса
     pg.init()
     win_size = (1000, 900)
     sc = pg.display.set_mode(win_size)
     run = True
-    player = Player(sc, win_size)
+    player = Player(sc, win_size, 50, [1, 0], 10)
     clock = pg.time.Clock()
+
+    platforms = [pg.Rect(0, 590, 100, 100), pg.Rect(150, 550, 5000, 100)]
 
     all_sprites = pg.sprite.Group()
     sprite = pg.sprite.Sprite()
@@ -90,25 +96,30 @@ if __name__ == '__main__':  # демонстрация работы класса
 
     katana = Katana(player.pos_x, player.pos_y, sprite)
 
+    jump = False
     while run:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 run = False
 
             elif event.type == pg.KEYDOWN:
-                if event.key == pg.K_SPACE:
-                    player.isJump = True
+                if event.key == pg.K_SPACE and not jump:
+                    jump = True
+                    player.check_gravity(platforms, True)
+                    jump = False
                 elif event.key == pg.K_f:
-                    player.shot()
+                    player.shot()                 
                 elif event.key == pg.K_LSHIFT:
                     player.lunge()
                 elif event.key == pg.K_e:
                     player.attack()
 
         sc.fill((0, 0, 0))
-        player.jump()
+        for platform in platforms:
+            pg.draw.rect(sc, pg.Color('blue'), platform)
+
         player.move()
-        player.render()
+        player.render(platforms)
         all_sprites.draw(sc)
         pg.display.flip()
         clock.tick(30)
